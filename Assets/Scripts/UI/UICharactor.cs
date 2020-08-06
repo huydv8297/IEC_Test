0052a7ed556229189e954aaf8d9d1245c343f195
huydv8297 <huydv8297@gmail.com>
2020-08-06 18:55:37 +0700

    Change cat charactor by Arisa

---
+++ Assets/Scripts/UI/UICharactor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICharactor : MonoBehaviour
+{
+
+    [SerializeField] GameObject charactorPos;
+    private void OnEnable() {
+        charactorPos.SetActive(true);
+    }
+
+    private void OnDisable() {
+        charactorPos.SetActive(false);
+    }
+}
