7be1c0f8ab19a7ef6ce4a7c3a7de06f5eba35885
huydv8297 <huydv8297@gmail.com>
2020-08-06 15:56:27 +0700

    Scale bottom button according to the screen ratio

---
+++ Assets/Scripts/UIScaler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScaler : MonoBehaviour
+{
+    [SerializeField] private Transform buttonContainerTransform;
+    [SerializeField] private float normalScaleValue = 1080f / 1920f;
+    [SerializeField] private float scaleValue;
+
+    void Awake()
+    {
+        scaleValue = (float) Screen.width / Screen.height;
+        scaleValue = (float) scaleValue / normalScaleValue;
+        if(scaleValue > 1)
+            scaleValue = 1;
+        buttonContainerTransform.localScale = new Vector3(scaleValue, scaleValue, 1f);
+    }
+
+}
